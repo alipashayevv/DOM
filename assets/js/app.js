871ea193b320fcc8fb61af453a6d7603c6002d90
Ali Pashayev <apashayev14102@ada.edu.az>
2022-10-18 00:48:06 +0400

    hw

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const a = document.getElementsByTagName('a')
+// for (const a of all) {
+//     a onclick = 
+// }
+
+addEventListener('a', function(){
+ const allsq = document.querySelectorAll('.grid-container');
+ this.document.grid-container.backgroundColor == 'red';
+});
